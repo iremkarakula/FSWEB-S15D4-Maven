44eb2df9713fe36e9faf35c4187c7e66516da77c
iremkarakula <irem123458@gmail.com>
2024-12-30 19:48:20 +0300

    project done

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,46 @@
 package org.example;
 
 
+import java.util.LinkedList;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+
+    }
+    public static boolean checkForPalindrome(String text){
+
+        String newText = text.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+
+        LinkedList<Character> charList = new LinkedList<>();
+
+        for(char c : newText.toCharArray()){
+            charList.add(c);
+        }
+
+        while (charList.size()>1){
+            if(!charList.pollFirst().equals(charList.pollLast())){
+                return false;
+            }
+        }
+
+        return true;
+
     }
 
+    public static String convertDecimalToBinary(int decimal){
+        LinkedList<Integer> binaryList = new LinkedList<>();
+        while(decimal > 0){
+            int remainder = decimal % 2;
+            binaryList.addFirst(remainder);
+            decimal = decimal / 2;
+        }
+
+        StringBuilder binaryStr = new StringBuilder();
+        for(int digit : binaryList){
+            binaryStr.append(digit);
+        }
+        return binaryStr.toString();
+    }
 }
